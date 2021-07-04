4a22fefbde7e89ee5f1536dfdc5851dcd4d89224
Ritu Singh <ritusingh941017@gmail.com>
2021-07-05 02:38:59 +0530

    uncomment

--- routes/candidate.route.js
+++ routes/candidate.route.js
@@ -6,6 +6,6 @@ var router = express.Router();
 
 router.post("/register", candidateController.register);
 router.post("/login", candidateController.login);
-//router.post("/candidate/:canID/job/:jobId", candidateController.jobApply)
+//router.post("/:canID/job/:jobId", candidateController.jobApply)
 
 module.exports = router;
--- routes/recuiter.route.js
+++ routes/recuiter.route.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 router.post("/register", recruiterController.register);
 router.post("/login", recruiterController.login);
-//router.get("/job/view-candidates/:id", recruiterController.viewCandidate);
+router.get("/job/view-candidates/:id", recruiterController.viewCandidate);
 
 
 module.exports = router;
